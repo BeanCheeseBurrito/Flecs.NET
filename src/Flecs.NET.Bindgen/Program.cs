665f7218e10128535be8e47d95df972b999058f9
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-08-26 20:45:27 -0700

    Add system include path for macos

--- src/Flecs.NET.Bindgen/Program.cs
+++ src/Flecs.NET.Bindgen/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Bindgen.NET;
 
 BindingOptions bindingOptions = new()
@@ -27,6 +28,9 @@ BindingOptions bindingOptions = new()
     GenerateExternVariables = true
 };
 
+if (OperatingSystem.IsMacOS())
+    bindingOptions.SystemIncludeDirectories.Add(GetMacOsHeaders());
+
 BindingGenerator.Generate(bindingOptions);
 
 string GetFlecsHeaderPath([CallerFilePath] string filePath = "")
@@ -37,4 +41,29 @@ string GetFlecsHeaderPath([CallerFilePath] string filePath = "")
 string GetBindingsOutputPath([CallerFilePath] string filePath = "")
 {
     return Path.GetFullPath(Path.Combine(filePath, "..", "..", "Flecs.NET.Bindings", "Flecs.g.cs"));
+}
+
+string GetMacOsHeaders()
+{
+    using Process process = new()
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "xcrun",
+            Arguments = "--sdk macosx --show-sdk-path",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+        }
+    };
+
+    process.Start();
+    process.WaitForExit();
+
+    string path = process.StandardOutput.ReadToEnd();
+
+    if (!Directory.Exists(path))
+        throw new DirectoryNotFoundException("Couldn't find system headers. Install XCode.");
+
+    return path + "/usr/include";
 }
