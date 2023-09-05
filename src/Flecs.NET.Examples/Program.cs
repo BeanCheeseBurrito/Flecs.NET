45fa0dbe79f104e4f635883a7886282129fad023
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-09-04 19:57:00 -0700

    Delete temp statement from example

--- src/Flecs.NET.Examples/Program.cs
+++ src/Flecs.NET.Examples/Program.cs
@@ -3,5 +3,4 @@
 Console.WriteLine("To run an example, use \"dotnet run --project Flecs.NET.Examples -p\"");
 Console.WriteLine("Example: \"dotnet run --project src/Flecs.NET.Examples --property:Example=Cpp_Entities_Basics\"");
 
-RandomNamespace.Random.Temp();
 #endif
