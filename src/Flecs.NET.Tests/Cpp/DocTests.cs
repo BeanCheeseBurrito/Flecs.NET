df4abd8cfffce3b23e90ad1bfe045d567443a635
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-09-09 04:11:44 -0700

    Port C++ doc tests and namespace

--- src/Flecs.NET.Tests/Cpp/DocTests.cs
+++ src/Flecs.NET.Tests/Cpp/DocTests.cs
@@ -1,12 +1,82 @@
 using Flecs.NET.Core;
+using Xunit;
+using static Flecs.NET.Bindings.Native;
 
 namespace Flecs.NET.Tests.Cpp
 {
-    public unsafe class DocTests
+    public class DocTests
     {
         public DocTests()
         {
             FlecsInternal.Reset();
+        }
+
+        [Fact]
+        private void SetBrief()
+        {
+            using World world = World.Create();
+
+            Entity e = world.Entity("Foo");
+
+            e.SetDocBrief("A brief description");
+
+            Assert.True(e.Has<EcsDocDescription>(Ecs.Doc.Brief));
+
+            Assert.Equal("A brief description", e.DocBrief());
+        }
+
+        [Fact]
+        private void SetName()
+        {
+            using World world = World.Create();
+
+            Entity e = world.Entity("Foo");
+
+            e.SetDocName("A name");
+
+            Assert.True(e.Has<EcsDocDescription>(Ecs.Name));
+
+            Assert.Equal("A name", e.DocName());
+        }
+
+        [Fact]
+        private void SetLink()
+        {
+            using World world = World.Create();
+
+            Entity e = world.Entity("Foo");
+
+            e.SetDocLink("A link");
+
+            Assert.True(e.Has<EcsDocDescription>(Ecs.Doc.Link));
+
+            Assert.Equal("A link", e.DocLink());
+        }
+
+        [Fact]
+        private void SetColor()
+        {
+            using World world = World.Create();
+
+            Entity e = world.Entity("Foo");
+
+            e.SetDocColor("A color");
+
+            Assert.True(e.Has<EcsDocDescription>(Ecs.Doc.Color));
+
+            Assert.Equal("A color", e.DocColor());
+        }
+
+        [Fact]
+        private void GetNameNoDocName()
+        {
+            using World world = World.Create();
+
+            Entity e = world.Entity("Foo");
+
+            Assert.True(!e.Has<EcsDocDescription>(Ecs.Name));
+
+            Assert.Equal("Foo", e.DocName());
         }
     }
 }
--- src/Flecs.NET/Core/Ecs.cs
+++ src/Flecs.NET/Core/Ecs.cs
@@ -493,4 +493,34 @@ namespace Flecs.NET.Core
         /// </summary>
         public static ref ulong PostFrame => ref EcsPostFrame;
     }
+
+    // Static classes
+    public static partial class Ecs
+    {
+        /// <summary>
+        ///     Utilities for documenting entities, components and systems.
+        /// </summary>
+        public static class Doc
+        {
+            /// <summary>
+            ///     Reference to <see cref="EcsDocBrief"/>.
+            /// </summary>
+            public static ref ulong Brief => ref EcsDocBrief;
+
+            /// <summary>
+            ///     Reference to <see cref="EcsDocDetail"/>.
+            /// </summary>
+            public static ref ulong Detail => ref EcsDocDetail;
+
+            /// <summary>
+            ///     Reference to <see cref="EcsDocLink"/>.
+            /// </summary>
+            public static ref ulong Link => ref EcsDocLink;
+
+            /// <summary>
+            ///     Reference to <see cref="EcsDocColor"/>.
+            /// </summary>
+            public static ref ulong Color => ref EcsDocColor;
+        }
+    }
 }
