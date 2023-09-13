ca848886f8725aa34ba78c5fa38026c71004fd45
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-09-12 23:16:26 -0700

    Update module lookup paths

--- src/Flecs.NET/Modules/Alerts.cs
+++ src/Flecs.NET/Modules/Alerts.cs
@@ -18,10 +18,10 @@ namespace Flecs.NET.Core
             {
                 FlecsAlertsImport(world);
 
-                world.Entity<Alert>("::flecs::alerts::Alert");
-                world.Entity<Info>("::flecs::alerts::Info");
-                world.Entity<Warning>("::flecs::alerts::Warning");
-                world.Entity<Err>("::flecs::alerts::Error");
+                world.Entity<Alert>("global::flecs.alerts.Alert");
+                world.Entity<Info>("global::flecs.alerts.Info");
+                world.Entity<Warning>("global::flecs.alerts.Warning");
+                world.Entity<Err>("global::flecs.alerts.Error");
             }
 
             /// <summary>
