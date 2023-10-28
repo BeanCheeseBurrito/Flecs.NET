eb96921fad589cc260e1a9e74daa1bf507e94e52
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-10-27 17:48:33 -0700

    Update build.zig file

--- src/Flecs.NET.Native/build.zig
+++ src/Flecs.NET.Native/build.zig
@@ -2,8 +2,15 @@ const std = @import("std");
 const Build = std.build;
 
 pub fn compileFlecs(b: *Build, lib: *Build.Step.Compile) void {
-    lib.addCSourceFile(.{ .file = std.build.LazyPath.relative("../../submodules/flecs/flecs.c"), .flags = &.{} });
+    lib.addCSourceFile(.{ .file = Build.LazyPath.relative("../../submodules/flecs/flecs.c"), .flags = &.{} });
     lib.linkLibC();
+
+    if (lib.optimize == .Debug) {
+        lib.strip = false;
+    } else {
+        lib.defineCMacro("NDEBUG", null);
+        lib.strip = true;
+    }
 
     if (lib.target.isWindows()) {
         lib.linkSystemLibraryName("ws2_32");
