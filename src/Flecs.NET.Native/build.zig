3924564117d252f99d7e68e18bcd4b6aa0938b61
BeanCheeseBurrito <110511593+BeanCheeseBurrito@users.noreply.github.com>
2023-11-09 12:17:46 -0800

    Add IOS support to zig build script and CI (#16)
    * Move github artifacts upload to macos runner
    
    * Try ios build
    
    * Set sys root to sdk path
    
    * Try relative path
    
    * Fix include?
    
    * Add ios simulator build and pack ios libraries
    
    * Add ios simulator path to CI
    
    * Fix incorrect branch boolean
    
    * Forgor dollar sign
    
    * Build for x64, update props and targets files, build as deterministic, reformat stuff
    
    * Remove ios-x64
    
    * Add ContinuousIntegrationBuild property to all projects

--- src/Flecs.NET.Native/build.zig
+++ src/Flecs.NET.Native/build.zig
@@ -1,19 +1,36 @@
 const std = @import("std");
 const Build = std.build;
 
-pub fn compileFlecs(b: *Build, lib: *Build.Step.Compile) void {
+pub const Options = struct {
+    ios_sdk_path: ?[]const u8 = null,
+    ios_simulator_sdk_path: ?[]const u8 = null,
+};
+
+pub fn compileFlecs(options: Options, b: *Build, lib: *Build.Step.Compile) void {
     lib.addCSourceFile(.{ .file = Build.LazyPath.relative("../../submodules/flecs/flecs.c"), .flags = &.{} });
     lib.linkLibC();
+    lib.strip = lib.optimize != .Debug;
 
-    if (lib.optimize == .Debug) {
-        lib.strip = false;
-    } else {
+    if (lib.optimize != .Debug) {
         lib.defineCMacro("NDEBUG", null);
-        lib.strip = true;
     }
 
-    if (lib.target.isWindows()) {
-        lib.linkSystemLibraryName("ws2_32");
+    switch (lib.target.getOsTag()) {
+        .windows => {
+            lib.linkSystemLibraryName("ws2_32");
+        },
+        .ios => {
+            lib.addSystemIncludePath(.{ .path = "/usr/include" });
+
+            if (lib.target.getAbi() == .simulator and options.ios_simulator_sdk_path != null) {
+                b.sysroot = options.ios_simulator_sdk_path;
+            } else if (options.ios_sdk_path != null) {
+                b.sysroot = options.ios_sdk_path;
+            } else {
+                @panic("A path to an IOS SDK needs to be provided when compiling for IOS.");
+            }
+        },
+        else => {},
     }
 
     b.installArtifact(lib);
@@ -23,7 +40,8 @@ pub fn build(b: *Build) void {
     const name = "flecs";
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const options = Options{ .ios_sdk_path = b.option([]const u8, "ios-sdk-path", "Path to an IOS SDK."), .ios_simulator_sdk_path = b.option([]const u8, "ios-simulator-sdk-path", "Path to an IOS simulator SDK.") };
 
-    compileFlecs(b, b.addSharedLibrary(.{ .name = name, .target = target, .optimize = optimize }));
-    compileFlecs(b, b.addStaticLibrary(.{ .name = name, .target = target, .optimize = optimize }));
+    compileFlecs(options, b, b.addSharedLibrary(.{ .name = name, .target = target, .optimize = optimize }));
+    compileFlecs(options, b, b.addStaticLibrary(.{ .name = name, .target = target, .optimize = optimize }));
 }
