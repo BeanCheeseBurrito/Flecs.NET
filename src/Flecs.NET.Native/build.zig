92be710a147ed0fe9cbe0d19468166ba679459a7
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-11-09 02:25:22 -0800

    Try ios build

--- src/Flecs.NET.Native/build.zig
+++ src/Flecs.NET.Native/build.zig
@@ -1,19 +1,32 @@
 const std = @import("std");
 const Build = std.build;
 
-pub fn compileFlecs(b: *Build, lib: *Build.Step.Compile) void {
+pub const Options = struct {
+    ios_sdk_path: ?[]const u8 = null,
+    ios_simulator_sdk_path: ?[]const u8 = null,
+};
+
+pub fn compileFlecs(options: Options, b: *Build, lib: *Build.Step.Compile) void {
     lib.addCSourceFile(.{ .file = Build.LazyPath.relative("../../submodules/flecs/flecs.c"), .flags = &.{} });
     lib.linkLibC();
+    lib.strip = lib.optimize != .Debug;
 
-    if (lib.optimize == .Debug) {
-        lib.strip = false;
-    } else {
+    if (lib.optimize != .Debug) {
         lib.defineCMacro("NDEBUG", null);
-        lib.strip = true;
     }
 
-    if (lib.target.isWindows()) {
-        lib.linkSystemLibraryName("ws2_32");
+    switch (lib.target.getOsTag()) {
+        .windows => {
+            lib.linkSystemLibraryName("ws2_32");
+        },
+        .ios => {
+            if (options.ios_sdk_path) |ios_sdk_path| {
+                lib.addSystemIncludePath(.{ .path = b.fmt("{s}/usr/include", .{ios_sdk_path}) });
+            } else {
+                @panic("A path to an IOS SDK needs to be provided when compiling for IOS.");
+            }
+        },
+        else => {},
     }
 
     b.installArtifact(lib);
@@ -23,7 +36,8 @@ pub fn build(b: *Build) void {
     const name = "flecs";
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const options = Options{ .ios_sdk_path = b.option([]const u8, "ios-sdk-path", "Path to an IOS SDK."), .ios_simulator_sdk_path = b.option([]const u8, "ios-simulator-sdk-path", "Path to an IOS simulator SDK.") };
 
-    compileFlecs(b, b.addSharedLibrary(.{ .name = name, .target = target, .optimize = optimize }));
-    compileFlecs(b, b.addStaticLibrary(.{ .name = name, .target = target, .optimize = optimize }));
+    compileFlecs(options, b, b.addSharedLibrary(.{ .name = name, .target = target, .optimize = optimize }));
+    compileFlecs(options, b, b.addStaticLibrary(.{ .name = name, .target = target, .optimize = optimize }));
 }
