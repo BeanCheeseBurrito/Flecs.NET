86204c1d2ed197c2f2c432380a63eac2021d90fb
Chris Parker <seep@sent.com>
2023-11-12 12:32:50 -0500

    Add Flecs.NET.UnityShims project

---
+++ src/Flecs.NET.UnityShims/UnityShims.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS8618 // Non-nullable property should contain value when exiting constructor
+
+using System;
+
+namespace UnityEngine
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class RuntimeInitializeOnLoadMethodAttribute : Attribute
+    {
+        public RuntimeInitializeOnLoadMethodAttribute(RuntimeInitializeLoadType _) {}
+    }
+
+    public enum RuntimeInitializeLoadType
+    {
+        SubsystemRegistration
+    }
+
+    public static class Application
+    {
+        public static string dataPath { get; }
+    }
+
+    public static class Debug
+    {
+        public static void LogError(string _) {}
+    }
+}
