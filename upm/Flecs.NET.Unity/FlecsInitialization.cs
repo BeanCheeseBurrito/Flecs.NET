1936f2c0332562a4a716b9269d35aae11bf20aff
Chris Parker <seep@sent.com>
2023-11-12 12:36:39 -0500

    Add upm folder, unmask all upm files from gitignore

---
+++ upm/Flecs.NET.Unity/FlecsInitialization.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Flecs.NET.Bindings;
+using UnityEngine;
+
+namespace Flecs.NET.Unity
+{
+    internal static class FlecsInitialization
+    {
+        private const string PackageName = "flecs";
+
+#if FLECS_DEBUG
+        private const string PackageRuntimesRoot = "Flecs.NET.Native/Debug";
+#else
+        private const string PackageRuntimesRoot = "Flecs.NET.Native/Release";
+#endif
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Initialize()
+        {
+#if UNITY_EDITOR
+            Native.BindgenInternal.DllFilePaths.InsertRange(0, EditorPackagePaths());
+#else
+            Native.BindgenInternal.DllFilePaths.InsertRange(0, RuntimePackagePaths());
+#endif
+
+            Native.BindgenInternal.ResolveLibrary();
+
+            if (Native.BindgenInternal._libraryHandle == IntPtr.Zero) // TODO move to BindgenInternal#IsLibraryResolved?
+            {
+                Debug.LogError("Failed to initialize Flecs.NET: unable to find valid flecs library for platform.");
+            }
+        }
+
+        private static string[] EditorPackagePaths()
+        {
+            const string import = Native.BindgenInternal.DllImportPath;
+
+            return new []
+            {
+                Path.GetFullPath($"Packages/{PackageName}/{PackageRuntimesRoot}/runtimes/linux-x64/native/{import}"),
+                Path.GetFullPath($"Packages/{PackageName}/{PackageRuntimesRoot}/runtimes/win-x64/native/{import}"),
+            };
+        }
+
+        private static string[] RuntimePackagePaths()
+        {
+            const string import = Native.BindgenInternal.DllImportPath;
+
+            return new []
+            {
+                $"{Application.dataPath}/Plugins/x86_64/{import}",
+                $"{Application.dataPath}/Plugins/{import}",
+            };
+        }
+    }
+
+}
