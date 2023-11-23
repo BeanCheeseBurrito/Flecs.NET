2e1f8556430026a8a873d5a2a692db424e2b615d
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-11-22 21:04:27 -0800

    Add soft assert option to zig build script

--- src/Flecs.NET.Native/build.zig
+++ src/Flecs.NET.Native/build.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Build = std.build;
 
 pub const Options = struct {
+    soft_assert: bool = false,
     ios_sdk_path: ?[]const u8 = null,
     ios_simulator_sdk_path: ?[]const u8 = null,
 };
@@ -13,6 +14,10 @@ pub fn compileFlecs(options: Options, b: *Build, lib: *Build.Step.Compile) void 
 
     if (lib.optimize != .Debug) {
         lib.defineCMacro("NDEBUG", null);
+    }
+
+    if (options.soft_assert) {
+        lib.defineCMacro("FLECS_SOFT_ASSERT", null);
     }
 
     switch (lib.target.getOsTag()) {
@@ -40,7 +45,7 @@ pub fn build(b: *Build) void {
     const name = "flecs";
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const options = Options{ .ios_sdk_path = b.option([]const u8, "ios-sdk-path", "Path to an IOS SDK."), .ios_simulator_sdk_path = b.option([]const u8, "ios-simulator-sdk-path", "Path to an IOS simulator SDK.") };
+    const options = Options{ .soft_assert = b.option(bool, "soft-assert", "Compile with the FLECS_SOFT_ASSERT define.") orelse false, .ios_sdk_path = b.option([]const u8, "ios-sdk-path", "Path to an IOS SDK."), .ios_simulator_sdk_path = b.option([]const u8, "ios-simulator-sdk-path", "Path to an IOS simulator SDK.") };
 
     compileFlecs(options, b, b.addSharedLibrary(.{ .name = name, .target = target, .optimize = optimize }));
     compileFlecs(options, b, b.addStaticLibrary(.{ .name = name, .target = target, .optimize = optimize }));
