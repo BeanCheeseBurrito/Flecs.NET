ea7307332595ed0ff39200f7f91e33100693ae7f
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2023-12-18 00:52:43 -0800

    Use actual C# primitives instead of aliasing built-in flecs entities

--- src/Flecs.NET/Modules/Meta.cs
+++ src/Flecs.NET/Modules/Meta.cs
@@ -1,4 +1,5 @@
 using System;
+using static Flecs.NET.Bindings.Native;
 
 namespace Flecs.NET.Core
 {
@@ -15,29 +16,17 @@ namespace Flecs.NET.Core
             /// <param name="world"></param>
             public readonly void InitModule(ref World world)
             {
-                Type<bool>.SetSymbolName("bool");
-                Type<byte>.SetSymbolName("u8");
-                Type<ushort>.SetSymbolName("u16");
-                Type<uint>.SetSymbolName("u32");
-                Type<ulong>.SetSymbolName("u64");
-                Type<sbyte>.SetSymbolName("i8");
-                Type<short>.SetSymbolName("i16");
-                Type<int>.SetSymbolName("i32");
-                Type<long>.SetSymbolName("i64");
-                Type<float>.SetSymbolName("f32");
-                Type<double>.SetSymbolName("f64");
-
-                world.Entity<bool>("::flecs.meta.bool");
-                world.Entity<byte>("::flecs.meta.u8");
-                world.Entity<ushort>("::flecs.meta.u16");
-                world.Entity<uint>("::flecs.meta.u32");
-                world.Entity<ulong>("::flecs.meta.u64");
-                world.Entity<sbyte>("::flecs.meta.i8");
-                world.Entity<short>("::flecs.meta.i16");
-                world.Entity<int>("::flecs.meta.i32");
-                world.Entity<long>("::flecs.meta.i64");
-                world.Entity<float>("::flecs.meta.f32");
-                world.Entity<double>("::flecs.meta.f64");
+                world.Component<bool>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsBool });
+                world.Component<byte>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsU8 });
+                world.Component<ushort>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsU16 });
+                world.Component<uint>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsU32 });
+                world.Component<ulong>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsU64 });
+                world.Component<sbyte>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsI8 });
+                world.Component<short>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsI16 });
+                world.Component<int>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsI32 });
+                world.Component<long>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsI64 });
+                world.Component<float>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsF32 });
+                world.Component<double>().Entity.Set(new EcsPrimitive { kind = ecs_primitive_kind_t.EcsF64 });
 
                 // TODO: Add support for string, char and native sized integers
             }
