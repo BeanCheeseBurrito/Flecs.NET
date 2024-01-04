d075414d225c6e102920c574e2dbebc2eba95810
BeanCheeseBurrito <mrbeancheeseburrito@protonmail.com>
2024-01-04 14:49:28 -0800

    Fix term count assertion on observers that use string queries

--- src/Flecs.NET/Core/ObserverBuilder.cs
+++ src/Flecs.NET/Core/ObserverBuilder.cs
@@ -162,7 +162,7 @@ namespace Flecs.NET.Core
             observerContext[0] = ObserverContext;
             BindingContext.SetCallback(ref observerContext->Iterator, userCallback, storeFunctionPointer);
 
-            Ecs.Assert(FilterBuilder.Terms.Count > 0, "Observers require at least 1 term.");
+            Ecs.Assert(FilterBuilder.Terms.Count > 0 || FilterBuilder.Desc.expr != null, "Observers require at least 1 term.");
             Ecs.Assert(EventCount != 0,
                 "Observer cannot have zero events. Use ObserverBuilder.Event() to add events.");
 
